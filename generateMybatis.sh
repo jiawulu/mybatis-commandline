39caba1c4021515a008ab92126cff7e89ffa0b18
lujiawu <jiawu.lu@gmail.com>
2017-12-04 09:49:49 +0800

    init

---
+++ generateMybatis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "http://www.mybatis.org/generator/configreference/xmlconfig.html"
+
+##java -jar mybatis-generator-core-x.x.x.jar -configfile generatorConfig.xml
+##java -jar mybatis-generator-core-x.x.x.jar -configfile generatorConfig.xml -overwrite
+##java -cp mybatis-generator-core-x.x.x.jar org.mybatis.generator.api.ShellRunner -configfile generatorConfig.xml
+##java -cp mybatis-generator-core-x.x.x.jar org.mybatis.generator.api.ShellRunner -configfile generatorConfig.xml -overwrite
+
+rm -rf gen
+mkdir gen
+
+java -cp "libs/*" org.mybatis.generator.api.ShellRunner -configfile generatorConfig.xml -overwrite
